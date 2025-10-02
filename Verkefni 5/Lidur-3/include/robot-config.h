803182764a3ed1d13bfe8fbb07a273fd085a7483
NidaleNieve <nidale16@outlook.com>
2025-10-02 11:14:14 +0000

    Bætti við öllum mótorum

--- Verkefni 5/Lidur-3/include/robot-config.h
+++ Verkefni 5/Lidur-3/include/robot-config.h
@@ -4,9 +4,23 @@ extern brain Brain;
 
 // VEXcode devices
 extern smartdrive Drivetrain;
+extern motor ArmMotor;
 extern gps GPS9;
+extern vision Vision5;
+extern sonar RangeFinderE;
+
 extern bumper BumperH;
 extern controller Controller1;
+
+
+using signature = vision::signature;
+extern signature Vision5__BLUEBOX;
+extern signature Vision5__GREENBOX;
+extern signature Vision5__REDBOX;
+extern signature Vision5__SIG_4;
+extern signature Vision5__SIG_5;
+extern signature Vision5__SIG_6;
+extern signature Vision5__SIG_7;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
--- Verkefni 5/Lidur-3/src/robot-config.cpp
+++ Verkefni 5/Lidur-3/src/robot-config.cpp
@@ -11,12 +11,31 @@ brain  Brain;
 motor LeftDriveSmart = motor(PORT1, ratio18_1, true);
 motor RightDriveSmart = motor(PORT10, ratio18_1, false);
 inertial DrivetrainInertial = inertial(PORT2);
+
+motor ArmMotor = motor(PORT8, ratio18_1, false);
+
 gps GPS9 = gps(PORT9, 0.00, -240.00, mm, 180);
+vision Vision5 = vision(PORT5, 50, Vision5__BLUEBOX, Vision5__GREENBOX, Vision5__REDBOX, Vision5__SIG_4, Vision5__SIG_5, Vision5__SIG_6, Vision5__SIG_7);
+sonar RangeFinderE = sonar(Brain.ThreeWirePort.E);
 
 bumper BumperH = bumper(Brain.ThreeWirePort.H);
 controller Controller1 = controller(primary);
 
 smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 319.19, 320, 40, mm, 1);
+
+
+/*vex-vision-config:begin*/
+signature Vision5__BLUEBOX =
+    signature(1, -3441, -2785, -3113, 8975, 10355, 9665, 2.5, 0);
+signature Vision5__GREENBOX =
+    signature(2, -5767, -4965, -5366, -3803, -2861, -3332, 2.5, 0);
+signature Vision5__REDBOX =
+    signature(3, 8099, 8893, 8496, -1505, -949, -1227, 2.5, 0);
+signature Vision5__SIG_4 = signature(4, 0, 0, 0, 0, 0, 0, 2.5, 0);
+signature Vision5__SIG_5 = signature(5, 0, 0, 0, 0, 0, 0, 2.5, 0);
+signature Vision5__SIG_6 = signature(6, 0, 0, 0, 0, 0, 0, 2.5, 0);
+signature Vision5__SIG_7 = signature(7, 0, 0, 0, 0, 0, 0, 2.5, 0);
+/*vex-vision-config:end*/
 
 // VEXcode generated functions
 
